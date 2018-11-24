9a66e35f7435fa46a96e4fdbd5d55a98f7f06036
3inchtime <3inchtime@gmail.com>
2018-11-24 14:25:14 +0800

    add Video API

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,14 +1,23 @@
 package dbops
 
-import "log"
+import (
+	"database/sql"
+	"log"
+	"pilipili_server/api/defs"
+	"pilipili_server/api/utils"
+	"time"
+)
 
 func AddUserCredential(loginName string, pwd string) error {
 	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")
 	if err != nil {
 		return err
 	}
-	stmtIns.Exec(loginName, pwd)
-	stmtIns.Close()
+	_, err = stmtIns.Exec(loginName, pwd)
+	if err != nil{
+		return err
+	}
+	defer stmtIns.Close()
 	return nil
 }
 
@@ -20,8 +29,11 @@ func GetUserCredential(loginName string) (string, error)  {
 	}
 
 	var pwd string
-	stmtOut.QueryRow(loginName).Scan(&pwd)
-	stmtOut.Close()
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
+	if err != nil && err != sql.ErrNoRows {
+		return "", err
+	}
+	defer stmtOut.Close()
 	return  pwd, nil
 }
 
@@ -31,7 +43,68 @@ func DelectUser(loginName string, pwd string) error {
 		log.Printf("DeleteUser error: %s", err)
 		return err
 	}
-	stmtDel.Exec(loginName, pwd)
-	stmtDel.Close()
+	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+	defer stmtDel.Close()
 	return  nil
+}
+
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	vid, err := utils.NewUUID()
+	if err != nil{
+		return nil, err
+	}
+	t := time.Now()
+	ctime := t.Format("Jan 02 2006, 15:04:05")
+	stmtIns, err := dbConn.Prepare(" INSERT INTO video_info (id, author_id, name, display_ctime)VALUES(?, ?, ?, ?)")
+	if err != nil{
+		return nil, err
+	}
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil{
+		return nil, err
+	}
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: ctime}
+	defer stmtIns.Close()
+	return res, nil
+}
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare("SELECT author_id, name, display_ctime FROM video_info WHERE id=?")
+
+	var aid int
+	var dct string
+	var name string
+
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	if err != nil && err != sql.ErrNoRows{
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	defer stmtOut.Close()
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: dct}
+	return res, nil
+}
+
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id=?")
+	if err != nil{
+		return err
+	}
+
+	_, err =  stmtDel.Exec(vid)
+	if err != nil{
+		return err
+	}
+
+	defer stmtDel.Close()
+	return nil
 }
