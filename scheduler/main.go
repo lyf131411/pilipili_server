557f9bf16a30b423da32b077ab8f8f1bc58750d7
3inchtime <3inchtime@gmail.com>
2018-11-27 22:21:39 +0800

    add scheduler

---
+++ scheduler/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"pilipili_server/scheduler/taskrunner"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+	router.GET("/video-delete-recoard/:vid-id", vidDelRecHandler)
+	return router
+}
+func main() {
+	go taskrunner.Start()
+	r := RegisterHandlers()
+	http.ListenAndServe(":9001", r)
+}
